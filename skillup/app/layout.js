2ed3c677d702b9d07b3df3824b5e26bdd7772ba8
Mehakpreet01 <mehakpreet4006@gmail.com>
2026-08-04 13:50:09 +0530

    Restore the simple app layout

--- skillup/app/layout.js
+++ skillup/app/layout.js
@@ -1,11 +1,5 @@
 import "./globals.css";
-import Script from "next/script";
 import { GlobalProvider } from "../lib/GlobalContext";
-
-const supabasePublicConfig = {
-  url: process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-  anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
-};
 
 export const metadata = {
   title: "SkillUp Tracker",
@@ -15,13 +9,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        {supabasePublicConfig.url && supabasePublicConfig.anonKey && (
-          <Script id="supabase-config" strategy="beforeInteractive">
-            {`window.__SUPABASE_CONFIG__ = ${JSON.stringify(supabasePublicConfig)};`}
-          </Script>
-        )}
-      </head>
       <body>
         <GlobalProvider>
           {children}
